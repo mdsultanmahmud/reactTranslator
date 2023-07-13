687b21c314f74ab40785c298ba303c8a28670765
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-07-13 17:36:24 +0600

    get country code

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -2,22 +2,33 @@ import './Translator.css'
 import { GiSpeaker } from 'react-icons/gi'
 import { AiOutlineCopy } from 'react-icons/ai'
 import { BiTransfer } from 'react-icons/bi'
+
+import countries from '../data.js';
 import { useState } from 'react';
 const Translator = () => {
     const [output, setOutput] = useState("")
-    const [countries, setCountries] = useState([])
+    // const [selectLng, setSelectLng] = useState('English')
+    const [selectLngCode, setSelectLngCode] = useState('en-GB')
+    // const [toLng, setToLng] = useState('Bengali')
+    const [toLngCode, setToLngCode] = useState('bn-IN')
     const handleTranslation = (e) => {
         e.preventDefault()
+        const form = e.target 
+        const input = form.selectLanguage.value
         setOutput("Functionality is not addeded!!! It's underconstruction")
+        // console.log("selected lang", selectLng)
+        // console.log("output langauge", toLng);
+        console.log("input", input);
+        console.log("select langauge code", selectLngCode, "output language code", toLngCode);
     }
-    
+
     return (
         <div className='container'>
             <div className="content">
                 <form onSubmit={handleTranslation}>
                     <div className='selected-lang'>
-                        <textarea spellCheck={false} name="select-language" id="" placeholder='Enter Your Text' cols="40" rows="15"></textarea>
-                        <textarea spellCheck={false} name="output-language" id="" placeholder='Translation' defaultValue={output} cols="40" rows="15" readOnly></textarea>
+                        <textarea spellCheck={false} name="selectLanguage" id="" placeholder='Enter Your Text' cols="40" rows="15"></textarea>
+                        <textarea spellCheck={false} name="outputLanguage" id="" placeholder='Translation' defaultValue={output} cols="40" rows="15" readOnly></textarea>
                         <div className='option'>
                             <div>
                                 <GiSpeaker size={22} style={{ cursor: 'pointer' }} />
@@ -26,20 +37,23 @@ const Translator = () => {
                                 <AiOutlineCopy size={22} style={{ cursor: 'pointer' }} />
                             </div>
                             <div>
-                                <select name="" id="">
-                                    <option value="English">English</option>
-                                    <option value="English">English</option>
-                                    <option value="English">English</option>
+                                <select onChange={(e) => 
+                                setSelectLngCode(e.target.value)
+                                
+                                }>
+                                    {
+                                        Object.keys(countries).map((key, index) =><option key={index} value={key} selected={countries[key] == "English"}>{countries[key]}</option>)
+                                    }
                                 </select>
                             </div>
                             <div>
-                                <BiTransfer size={22} style={{ cursor: 'pointer' }} />
+                                <BiTransfer onClick={handleTranslation} size={22} style={{ cursor: 'pointer' }} />
                             </div>
                             <div>
-                                <select name="" id="">
-                                    <option value="English">English</option>
-                                    <option value="English">English</option>
-                                    <option value="English">English</option>
+                                <select onChange={(e) => setToLngCode(e.target.value)}>
+                                    {
+                                        Object.keys(countries).map((key, index) => <option key={index} value={key} selected={countries[key] == "Bengali"}>{countries[key]}</option>)
+                                    }
                                 </select>
                             </div>
                             <div>
