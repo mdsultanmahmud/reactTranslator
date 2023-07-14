fe8b14104b82a904b708fcdf0605cfd649b1fb09
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-07-14 16:10:28 +0600

    project done

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -11,10 +11,7 @@ const Translator = () => {
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
     const [isCopied, setIsCopied] = useState(false)
-
-    // const [selectLng, setSelectLng] = useState('English')
     const [selectLngCode, setSelectLngCode] = useState('en-GB')
-    // const [toLng, setToLng] = useState('Bengali')
     const [toLngCode, setToLngCode] = useState('bn-IN')
 
     useEffect(() => {
@@ -26,41 +23,83 @@ const Translator = () => {
     const handleTranslation = (e) => {
         e.preventDefault()
         setLoading(true)
-        console.log(formText.trim()) 
-        setOutput("Functionality is not addeded!!! It's underconstruction")
-        console.log("select langauge code", selectLngCode, "output language code", toLngCode);
+        setError('')
         if (!formText) {
             setError("You have not entered any text!!!")
         } else {
-            const api = `https://api.mymemory.translated.net/get?q=${formText}&langpair=${selectLngCode}|${toLngCode}`
+            const api = `https://api.mymemory.translated.net/get?q=${formText.trim()}&langpair=${selectLngCode}|${toLngCode}`
             fetch(api)
                 .then(res => res.json())
                 .then(data => {
-                    setOutput(data.responseData.translatedText)
-                    setLoading(false)
+                    if(data.responseData.translatedText == "" || data.responseData.translatedText == null){
+                        setError("No translated language is available!!")
+                    }else{
+                        setOutput(data.responseData.translatedText)
+                    }
+                    setLoading(false) 
                 })
         }
     }
 
-    const handleCopy = async(copy) =>{
-        if(copy == "from"){
+    const handleCopy = async (copy) => {
+        if (copy == "from") {
             try {
                 await navigator.clipboard.writeText(formText)
-                setIsCopied(true)
+                setIsCopied("from")
             } catch (error) {
                 console.log(error);
             }
-        }else{
+        } else {
             try {
                 await navigator.clipboard.writeText(output)
-                setIsCopied(true)
+                setIsCopied("output")
             } catch (error) {
                 console.log(error);
-
             }
         }
+        setTimeout(() => {
+            setIsCopied(false)
+        }, 1000);
     }
 
+    // const handleSpeak = (text) => {
+    //     let speakingText
+    //     if (text === 'from') {
+    //         speakingText = new SpeechSynthesisUtterance(formText)
+    //         speakingText.lang = selectLngCode
+    //     } else {
+    //         speakingText = new SpeechSynthesisUtterance(output)
+    //         speakingText.lang = toLngCode
+    //         console.log(toLngCode)
+    //     }
+    //     speechSynthesis.speak(speakingText)
+    //     const voices = speechSynthesis.getVoices()
+    //     console.log("voice exist..", voices);
+    // }
+    const handleSpeak = (text) => {
+        let speakingText;
+        
+        if (text === 'from') {
+          speakingText = new SpeechSynthesisUtterance(formText);
+          speakingText.lang = selectLngCode;
+        } else {
+          speakingText = new SpeechSynthesisUtterance(output);
+          speakingText.lang = toLngCode;
+        }
+        const voices = speechSynthesis.getVoices();
+        const selectedVoice = voices.find(voice => voice.lang === speakingText.lang);
+        if (selectedVoice) {
+          speakingText.voice = selectedVoice;
+          speechSynthesis.speak(speakingText);
+        } else {
+          setError("No voice available for the specified language!!")
+        }
+
+        setTimeout(() => {
+            setError("")
+        }, 2000);
+      };
+      
     return (
         <div className='container'>
             <div className="content">
@@ -70,15 +109,16 @@ const Translator = () => {
                             setFormText(e.target.value)
                             setError('')
                             setOutput('')
-                           
+
                         }
                         } spellCheck={false} name="selectLanguage" id="" placeholder='Enter Your Text' cols="40" rows="15"></textarea>
                         <textarea spellCheck={false} name="outputLanguage" id="" placeholder='Translation' defaultValue={loading ? "Translating..." : output} cols="40" rows="15" readOnly></textarea>
                         <div className='option'>
                             <div>
-                                <GiSpeaker size={22} style={{ cursor: 'pointer' }} />
+                                <GiSpeaker onClick={() => handleSpeak("from")} size={22} style={{ cursor: 'pointer' }} />
                             </div>
-                            <div>
+                            <div style={{ position: "relative" }}>
+                                {isCopied == 'from' && <p className='copy'>Copied</p>}
                                 <AiOutlineCopy onClick={() => handleCopy("from")} size={22} style={{ cursor: 'pointer' }} />
                             </div>
                             <div>
@@ -101,11 +141,13 @@ const Translator = () => {
                                     }
                                 </select>
                             </div>
-                            <div>
-                                <GiSpeaker size={22} style={{ cursor: 'pointer' }} />
+
+                            <div style={{ position: 'relative' }}>
+                                {isCopied == 'output'  && <p className='copy'>Copied</p>}
+                                <AiOutlineCopy onClick={() => handleCopy("to")} size={22} style={{ cursor: 'pointer' }} />
                             </div>
                             <div>
-                                <AiOutlineCopy onClick={() => handleCopy("to")} size={22} style={{ cursor: 'pointer' }} />
+                                <GiSpeaker onClick={() => handleSpeak("to")} size={22} style={{ cursor: 'pointer' }} />
                             </div>
                         </div>
                     </div>
